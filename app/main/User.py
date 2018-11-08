91423fa1e74d8593eaf324db13e8ecff478757d0
xiaoyuann <602632139@qq.com>
2018-11-08 20:13:06 +0800

    用户注册

--- app/main/User.py
+++ app/main/User.py
@@ -1,5 +1,19 @@
-from PTConnectionPool import PTConnectionPool
+from PTConnectionPool import PTConnectionPool, getPTConnection
+
 
 class User:
-    def registerUser(self,info_json):
-        sql = "insert "
+    def registerUser(self, info_json):
+        sql = "insert into user (Name,Ban,id,ReportNum,Password,BanTime,Banned,Photo,VoiceNum,FavoritesNum,EssayNum,PoetryNum,Vip,VipTime) " \
+              "values ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+                  info_json["Name"], info_json["Ban"], info_json["id"],
+                  info_json["ReportNum"], info_json["Password"], info_json["BanTime"],
+                  info_json["Banned"], info_json["Photo"], info_json["VoiceNum"],
+                  info_json["FavoritesNum"], info_json["EssayNum"], info_json["PoetryNum"],
+                  info_json["Vip"], info_json["VipTime"])
+        with getPTConnection() as db:
+            return db.cursor.execute(sql)
+
+    def searchMail(self, mail_id):
+        sql = "select * from user where user.id=%s" % mail_id
+        with getPTConnection() as db:
+            return db.cursor.execute(sql)
--- manage.py
+++ manage.py
@@ -1,24 +1,44 @@
 import tornado.ioloop
 import tornado.web
 from app.main.Poems import Poems
+from app.main.User import User
 
 
-class PoemsHandler(tornado.web.RequestHandler):
+class SelectPoemsHandler(tornado.web.RequestHandler):
     poems = Poems()
 
     def get(self):
+        self.set_header('content-type', 'application/json')
         poems_index = int(self.get_argument("poems_index"))
         self.write(self.poems.getPoems(poems_index))
 
 
-class UserHandler(tornado.web.RequestHandler):
-    poems=Poems()
+class RegisterUserHandler(tornado.web.RequestHandler):
+    user = User()
+
+    def get(self):
+        user_mail = self.get_argument('user_mail')
+        if self.user.searchMail(user_mail):
+            self.write("True")
+        else:
+            self.write("False")
+
+    def post(self):
+        user_mail = self.get_argument('user_mail')
+        user_password = self.get_argument('user_password')
+        info_json = {"Name": "青莲居士", 'Ban': 0, 'id': user_mail, 'ReportNum': 0, 'Password': user_password,
+                     'Banned': None, 'Photo': None, 'VoiceNum': 0, 'FavoritesNum': 0, 'EssayNum': 0, 'PoetryNum': 0,
+                     'Vip': 0, 'VipTime': None}
+        if self.user.registerUser(info_json):
+            self.write("True")
+        else:
+            self.write("False")
+
 
 def make_app():
     return tornado.web.Application([
-        (r"/poems", PoemsHandler),
-
-
+        (r"/poems", SelectPoemsHandler),
+        (r"/register", RegisterUserHandler),
     ])
 
 
